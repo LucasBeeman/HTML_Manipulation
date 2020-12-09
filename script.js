8165b7e53a26fb8a5f29bdf41a1e6892fd88eb4d
Lucas.B <70167892+LucasBeeman@users.noreply.github.com>
2020-12-09 13:14:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Enter your code below to edit index.html
+
+yourName = "Lucas Beemanm";
+document.getElementById("yourName").innerHTML = yourName;
+
+bio = "I am eat at least 3 times a day";
+document.getElementById("myBio").innerHTML = bio;
+
+collage = "The collage I would like to go to is Kent state.";
+document.getElementById("collegeChoice").innerHTML = collage;
+
+program = "I would like to study computer science.";
+document.getElementById("programChoice").innerHTML = program;
+
+program = "I chose computer science, because I want to be involve in a career that involvoes coding. I chose kent state because it's a good choice for computer science..";
+document.getElementById("collegeReason").innerHTML = program;
+
+program = "The career I want to be in is a software developer";
+document.getElementById("careerChoice").innerHTML = program;
+
+program = "The company I would like to work for is NVIDIA";
+document.getElementById("companyChoice").innerHTML = program;
+
+program = "I want to be a software developer for NVIDIA, because you get to work on the software aspect of their graphics cards, which works with C languages.";
+document.getElementById("careerReason").innerHTML = program;
+
+program = "JavaScript can output a to a page through the console, a popup, or embedded straight into the HTML page, like what I'm doing right now.";
+document.getElementById("answerTwo").innerHTML = program;
+
+program = "Javascript can change every attribute in HTML and even CSS";
+document.getElementById("answerThree").innerHTML = program;
